f3eeecdcd2d0da94b6b9bc42eb06832880377c43
Adrian Duong Szeto <adrianduongszeto@gmail.com>
2022-05-07 22:12:57 -0700

    search bar now sends data through console. Some api code was added as well.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,22 @@ console.log("Loading JS file");
 const api_key = '_HyU-LCjmmWczVkUDEHvN1KASN4jZDvg6HlZkt8v-JXQkD2iVhNFu6pnyTV-j37hlQEWO7wVe8nGKuY19V7zSlXaB93v485D79XlT0DIl1kD69R-ykuts-gW6hhvYnYx';
 
 
+
 const options = {
     method: 'GET',
     headers: {
         'Authorization': "Bearer" + api_key,
         'Content-Type': 'application/json',
-        "Access-Control-Allow-Origin": "https://api.yelp.com"
+        'Access-Control-Allow-Origin': "https://api.yelp.com"
     },
-    mode: 'no-cors'
+    mode: 'JSONP'
 };
 
+
+
+// Original API fetch
 var getUserRepos = function (user) {
-    var apiUrl = "https://api.yelp.com/v3/businesses/search?term=pizza&limit=10&sort_by=rating&location=" + place;
+    var apiUrl = "https://api.yelp.com/v3/businesses/search?term=pizza&limit=10&sort_by=rating&location=";
 
     fetch(apiUrl)
         .then(function (response) {
@@ -32,4 +36,12 @@ var getUserRepos = function (user) {
         });
 };
 
+getUserRepos();
+
+var searchEl = document.getElementById("query");
+
+document.querySelector('form.searchPizza').addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(searchEl.value);
+});
 
